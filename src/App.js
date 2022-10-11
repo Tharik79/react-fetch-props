f4aa4cb21d883db9d20aaf9e6d945f50c9111433
tharikuthuman <thaarik@gmail.com>
2022-10-11 11:53:55 +0530

    fetch map done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { React, useState, useEffect } from 'react';
+
 function App() {
+  const [posts, setPosts] = useState({});
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.json())
+      .then((data) => setPosts(data));
+    // console.log(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {posts.map((post) => {
+          return (
+            <div class="card">
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+
+                <p className="card-text">{post.body}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
